0d4a6d70d66516710388f6d42ab0ebf378913c40
tomathosauce <tomathosauce@users.noreply.github.com>
2023-07-21 05:41:37 -0500

    cambios

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 import socket
+import os
+
+hostname = socket.gethostname() if 'HOSTNAME' not in os.environ else os.environ['HOSTNAME']
 
 app = Flask(__name__)
 
+solicitudes = 0
+
 @app.route('/')
 def index():
-    return render_template('index.html', hostname=socket.gethostname())
+    return render_template('index.html', hostname=hostname, solicitudes=solicitudes)
 
 app.run(host='0.0.0.0', port=5000)
